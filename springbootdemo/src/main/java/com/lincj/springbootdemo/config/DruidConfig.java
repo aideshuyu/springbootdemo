ce9551a55b50614200a5ee61a05bf0c2c97673df
lincj <lincjzhku@qq.com>
2020-01-15 16:57:19 +0800

    配置druid数据源
    Signed-off-by: lincj <lincjzhku@qq.com>

---
+++ springbootdemo/src/main/java/com/lincj/springbootdemo/config/DruidConfig.java
@@ -0,0 +1,55 @@
+package com.lincj.springbootdemo.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+/**
+ * druid数据源的配置类
+ */
+@Configuration
+public class DruidConfig {
+
+
+    //声明数据源
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource") //从配置文件中读取spring.datasource开头的配置信息
+    public DataSource druidDataSource(){
+        return new DruidDataSource();
+    }
+
+    //实现web监控的配置处理
+    @Bean
+    public ServletRegistrationBean duridServlet(){
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new StatViewServlet() , "/druid/*");
+        //servletRegistrationBean.addInitParameter("allow", "127.0.0.1,192.168.202.233");//白名单
+        //servletRegistrationBean.addInitParameter("deny", "192.168.202.234");//黑名单
+        servletRegistrationBean.addInitParameter("loginUsername", "root");//用户名
+        servletRegistrationBean.addInitParameter("loginPassword", "root");//密码
+        servletRegistrationBean.addInitParameter("resetEnable", "false");//是否可以重置数据源
+        return servletRegistrationBean;
+    }
+
+    //监控
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean(){
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new WebStatFilter());
+        filterRegistrationBean.addUrlPatterns("/*");//所有请求进行监控处理
+        filterRegistrationBean.addInitParameter("exclusions", "/static/*,*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");//排除
+        return filterRegistrationBean;
+    }
+
+
+
+
+
+
+}
--- springbootdemo/src/main/java/com/lincj/springbootdemo/controller/IndexController.java
+++ springbootdemo/src/main/java/com/lincj/springbootdemo/controller/IndexController.java
@@ -4,15 +4,35 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.SQLOutput;
+
 @Controller
 public class IndexController {
+
+    @Resource
+    public DataSource dataSource;
 
     @RequestMapping("/")
     public String index(){
 //        ModelAndView modelAndView = new ModelAndView();
 //        modelAndView.setViewName("index");
 //        return modelAndView;
+        Connection connection = null;
+        try{
+            connection = dataSource.getConnection();
+            System.out.println(connection.isClosed());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+
+        }
         return "index";
     }
+
+
 
 }
